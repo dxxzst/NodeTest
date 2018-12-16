b52ba9de7c1609957229d6597b6637a7b99be186
song.xue@seek-data.com <song.xue@seek-data.com>
2018-12-16 22:11:40 +0800

    test

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const myRequest = require('./server/myRequest');
 
+//允许跨域
 app.all("*", function (req, res, next) {
     //设置允许跨域的域名，*代表允许任意域名跨域
     res.header("Access-Control-Allow-Origin", "*");
     //允许的header类型
-    res.header("Access-Control-Allow-Headers", "content-type");
+    res.header("Access-Control-Allow-Headers", "*");
     //跨域允许的请求方式
-    res.header("Access-Control-Allow-Methods", "DELETE,PUT,POST,GET,OPTIONS");
+    res.header("Access-Control-Allow-Methods", "*");
     if (req.method.toLowerCase() === 'options') {
         res.send(200);
     } else {
@@ -15,11 +17,28 @@ app.all("*", function (req, res, next) {
     }
 });
 
-app.get('/', function (req, res) {
-    res.send('Hello World');
+//提供静态资源
+app.use(express.static('web'));
+
+app.get('/*', function (req, res) {
+    if (req.url.indexOf('.pdf') !== -1) {
+        myRequest.requestPdf2(function (data) {
+            res.status(200).json(data);
+        }, req.originalUrl);
+    } else {
+        res.send(200);
+    }
 });
 
-var server = app.listen(3001, function () {
+app.post('/api/search/research_report_figure', function (req, res) {
+    req.on('data', function (data) {
+        myRequest.requestSearch(function (data) {
+            res.send(data);
+        }, data);
+    });
+});
+
+var server = app.listen(9872, function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log("应用实例，访问地址为 http://%s:%s", host, port)
