23119494b49f57cafb34119de1a8c018615310aa
XueSong <song.xue@seek-data.com>
2018-12-16 22:15:44 +0800

    Update myRequest.js

--- server/myRequest.js
+++ server/myRequest.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require('fs');
 var request = require('request');
 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoieWltZW5nIiwicGFzc3dvcmQiOiJ5aW1lbmciLCJ1c2VyVHlwZSI6InR5cGUxIiwiZXhwIjoxNTQ3MTc3Mzk3LCJpc3MiOiJnby5taWNyby5zcnYudXNlciJ9.1znZIOV6gvEp5-B-AfgkCqyHAAKwhlIvTc3-svMGoPI";
+const token = "";
 
 function requestSearch(callback, data) {
     let opt = {
